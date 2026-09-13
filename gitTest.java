11fdf7a4577233ec0a3035655dd162afe4db795c
Larry Yan <larry.yan136@gmail.com>
2026-09-13 15:42:32 -0400

    hi

--- gitTest.java
+++ gitTest.java
@@ -1,6 +1,6 @@
 public class gitTest {
     public static void main(String[]args){
-        int x = 59;
+        int x = 53;
         int y = 0;
         for (int i=0; i<45; i++){
             y+=x;
