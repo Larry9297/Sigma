a6acfa1f478106f1815567d66e7f9d3872758739
Larry9297 <larry.yan136@gmail.com>
2026-09-13 15:37:00 -0400

    x=50

--- gitTest.java
+++ gitTest.java
@@ -1,6 +1,6 @@
 public class gitTest {
     public static void main(String[]args){
-        int x = 49;
+        int x = 50;
         int y = 0;
         for (int i=0; i<40; i++){
             y+=x;
