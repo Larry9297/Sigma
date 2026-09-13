0814cdab91a26f41da77ec32aea42031aea539be
Larry9297 <larry.yan136@gmail.com>
2026-09-13 15:40:38 -0400

    Update gitTest.java

--- gitTest.java
+++ gitTest.java
@@ -2,7 +2,7 @@ public class gitTest {
     public static void main(String[]args){
         int x = 59;
         int y = 0;
-        for (int i=0; i<40; i++){
+        for (int i=0; i<45; i++){
             y+=x;
         }
         y*=x;
