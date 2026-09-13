1ae382dda257b2864580e03ffc2b2f9b3b53c36b
Larry Yan <larry.yan136@gmail.com>
2026-09-13 15:02:48 -0400

    49 gets added to itself 20 times. Then the result gets multiplied by 49.

---
+++ gitTest.java
@@ -0,0 +1,11 @@
+public class gitTest {
+    public static void main(String[]args){
+        int x = 49;
+        int y = 0;
+        for (int i=0; i<20; i++){
+            y+=x;
+        }
+        y*=x;
+        System.out.println(y);
+    } 
+}
