df1be2f8b2858168f591bb934184b2bcaa0f333a
Larry9297 <larry.yan136@gmail.com>
2026-09-13 15:24:29 -0400

    The loop loops 30 times instead of 20.

--- gitTest.java
+++ gitTest.java
@@ -2,7 +2,7 @@ public class gitTest {
     public static void main(String[]args){
         int x = 49;
         int y = 0;
-        for (int i=0; i<20; i++){
+        for (int i=0; i<30; i++){
             y+=x;
         }
         y*=x;
